5465b5a0f94de0b7e2f9f434e514b82007bfc50a
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-07-21 16:46:56 +0000

    [end]index_16

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
+  resources :books, only: [:new, :index, :show]
   get "/homes/about" => "homes#about", as: "about"
 
 end
